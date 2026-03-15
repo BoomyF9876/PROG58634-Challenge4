5143519e4f5e22c1760286efbaaf244bd6d79e94
BoomyF9876 <710676850@qq.com>
2026-03-15 03:31:49 -0400

    Add leaderboard, player name update skip playfab

--- Assets/Scripts/PokemonController.cs
+++ Assets/Scripts/PokemonController.cs
@@ -24,34 +24,97 @@ public class PokemonController : MonoBehaviour
         public PokemonSprite sprites;
     }
 
+    public TMP_Text LeaderBoardText;
+    public TMP_Text PlayerLabel;
     public TMP_Text PokemonName;
+    public TMP_InputField PlayerId;
     public TMP_InputField PokemonId;
     public Image PokemonImage;
     public Button PokemonBtn;
 
+    public Button PlayerUpdateBtn;
+
+    private string LeaderBoardPrefix;
+    private string PlayerName;
+
+    private Dictionary<string, int> leaderBoard = new Dictionary<string, int>();
+
     private void Start()
     {
-        PokemonInfo weatherInfo = GetPokemonById();
+        LeaderBoardPrefix = LeaderBoardText.text;
+        PlayerName = PlayerId.placeholder.GetComponent<TMP_Text>().text;
 
-        Debug.Log($"Name: {weatherInfo.name}, Id: {weatherInfo.id}, Image: {weatherInfo.sprites.front_default}");
+        LoadPokemon(PokemonId.placeholder.GetComponent<TMP_Text>().text);
 
-        //CityText.text = $"City: {weatherInfo.name}";
-        //WeatherText.text = $"{weatherInfo.weather[0].main} and {weatherInfo.weather[0].description}";
-        //TemperatureText.text = $"Temperature: {weatherInfo.main.temp - 273.15f}";
+        PokemonId.onSubmit.AddListener((value) =>
+        {
+            LoadPokemon(value);
+        });
 
-        //currentTimeDelay = updateTimeDelay;
+        PokemonBtn.onClick.AddListener(() =>
+        {
+            LoadLeaderBoard();
+        });
+
+        PlayerUpdateBtn.onClick.AddListener(() =>
+        {
+            PlayerName = PlayerId.text;
+            PlayerLabel.text = "Name: " + PlayerName;
+        });
     }
 
-    private PokemonInfo GetPokemonById()
+    private void LoadPokemon(string id)
     {
-        string id = PokemonId.text;
+        PokemonInfo pokemonInfo = GetPokemonById(id);
+        PokemonName.text = pokemonInfo.name;
+        StartCoroutine(LoadImage(pokemonInfo.sprites.front_default));
+    }
+
+    private PokemonInfo GetPokemonById(string id)
+    {
         string url = $"https://pokeapi.co/api/v2/pokemon/{id}/";
 
-        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-        StreamReader reader = new StreamReader(response.GetResponseStream());
-        string jsonResponse = reader.ReadToEnd();
+        try
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string jsonResponse = reader.ReadToEnd();
 
-        return JsonUtility.FromJson<PokemonInfo>(jsonResponse);
+            return JsonUtility.FromJson<PokemonInfo>(jsonResponse);
+        }
+        catch (Exception e)
+        {
+            Debug.LogError($"Unable to fetch Pokemon #{id}");
+            return null;
+        }
+    }
+
+    private IEnumerator LoadImage(string imageUrl)
+    {
+        using (WWW www = new WWW(imageUrl))
+        {
+            yield return www;
+            PokemonImage.sprite = Sprite.Create(www.texture, new Rect(0, 0, www.texture.width, www.texture.height), new Vector2(0.5f, 0.5f));
+        }
+    }
+
+    private void LoadLeaderBoard()
+    {
+        if (leaderBoard.ContainsKey(PlayerName))
+        {
+            leaderBoard[PlayerName]++;
+        }
+        else
+        {
+            leaderBoard.Add(PlayerName, 1);
+        }
+
+        int itr = 0;
+        LeaderBoardText.text = LeaderBoardPrefix;
+        foreach (var entry in leaderBoard)
+        {
+            LeaderBoardText.text += $"\n{itr++}. {entry.Key}: {entry.Value}";
+        }
     }
 }
